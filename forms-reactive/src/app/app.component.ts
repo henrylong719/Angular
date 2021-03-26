e6ad1664b88e45f1ef17ba7837639db055971796
henrylong <henrylong719@gmail.com>
2021-03-26 19:57:20 +1030

    add source code for reactive form

---
+++ forms-reactive/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent implements OnInit {
+  genders = ["male", "female"];
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      userData: new FormGroup({
+        // new FormControl(default value, validators)
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+
+      gender: new FormControl("male"),
+    });
+  }
+  onSubmit() {
+    console.log("tes");
+    console.log(this.signupForm);
+  }
+}
